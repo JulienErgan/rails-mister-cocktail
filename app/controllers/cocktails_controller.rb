15e7625f3ba3849fe43d3e0e982a289094eb17ee
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-14 20:16:44 +0900

    correction on seeding

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,20 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def cocktail_params
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# MANUAL SEEDING
+# MANUAL SEEDING for ingredients
 # Ingredient.create(name: "lemon")
 # Ingredient.create(name: "ice")
 # Ingredient.create(name: "mint leaves")
 
-# SEEDING WITH JSON
+# SEEDING WITH JSON for ingredients names
 require 'json'
 require 'open-uri'
 
+# CLEANING UP database of cocktails and associated doses
+# CLEANING UP database of ingredients - MIND the order of deletion
+# we need to delete the cocktails before deleting the ingredients
+puts 'Cleaning up the cocktails and the associated doses...'
+Cocktail.destroy_all
+puts 'Cleaning up the ingredients...'
+Ingredient.destroy_all
+
+# PARSING ingredients
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 # ingredients is an array of hashes
 ingredients = JSON.parse(open(url).read).first[1]
 
-puts 'Cleaning up the database...'
-Ingredient.destroy_all
-puts 'Creating ingredients...'
+# CREATING ingredients
+puts 'Creating the ingredients..'
 
 ingredients.each_with_index do |ingredient, index| # ingredient is a hash
   Ingredient.create!(
@@ -29,4 +37,22 @@ ingredients.each_with_index do |ingredient, index| # ingredient is a hash
   )
 end
 
-puts "Finished creating #{Ingredient.count} ingredients!"
+puts "Finished creating #{Ingredient.count} ingredients name!"
+
+# CREATING cocktails and doses
+puts 'Creating the cocktails and doses..'
+3.times do
+  cocktail = Cocktail.create!(
+    name: Faker::Name.first_name
+  )
+  2.times do |count|
+    Dose.create!(
+      description: "#{rand(0..5)} cl",
+      cocktail: cocktail,
+      ingredient: Ingredient.all[count]
+    )
+  end
+end
+
+puts "Finished creating #{Cocktail.count} cocktails!"
+puts "Finished creating #{Dose.count} doses!"
