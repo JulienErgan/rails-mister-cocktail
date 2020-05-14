e4b1a1cb1992d2817622a57f5aad2df68d3ce1ab
Julien Ergan <julien.ergan.code@gmail.com>
2020-05-14 15:32:48 +0900

    seeding completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+# MANUAL SEEDING
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+
+# SEEDING WITH JSON
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredients is an array of hashes
+ingredients = JSON.parse(open(url).read).first[1]
+
+puts 'Cleaning up the database...'
+Ingredient.destroy_all
+puts 'Creating ingredients...'
+
+ingredients.each_with_index do |ingredient, index| # ingredient is a hash
+  Ingredient.create!(
+    name: ingredients[index]['strIngredient1']
+  )
+end
+
+puts "Finished creating #{Ingredient.count} ingredients!"
