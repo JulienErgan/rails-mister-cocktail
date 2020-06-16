b2148c9ea6f33a9b3b4f7d44e5fed21e3ea25055
Julien Ergan <julien.ergan.code@gmail.com>
2020-06-16 16:57:35 +0900

    progress saved

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resource :doses, only: [:new, :create]
+    # get '/cocktails/:cocktail_id', to: 'doses#new', as: :new_cocktail_doses
+    # post '/cocktails/:cocktail_id', to: 'doses#create'
   end
   resources :doses, only: :destroy
 end
